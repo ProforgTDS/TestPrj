288c296462a5d701ec2a1c9212a2759121a825d2
ProforgTDS <formybusiness@mail.ru>
2016-10-04 12:22:06 +0300

    its ok

--- FirstTest/src/Main.java
+++ FirstTest/src/Main.java
@@ -13,10 +13,13 @@ public class Main {
     public static void main(String[] args) {
 
         Box mybox = new Box();
+        Box mybox2;
         mybox.higth = 10;
         mybox.wigth = 15;
+        mybox2 = mybox;
+        mybox = null;
         System.out.println("Hello World!");
-        System.out.print("size of box = " + mybox.higth * mybox.wigth);
+        System.out.print("size of box = " + mybox2.higth * mybox2.wigth);
     }
 
 }
