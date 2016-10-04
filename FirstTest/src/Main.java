b602852a2ad2acb22bf44f867409909131ab0dff
ProforgTDS <formybusiness@mail.ru>
2016-10-04 12:02:49 +0300

    its ok

--- FirstTest/src/Main.java
+++ FirstTest/src/Main.java
@@ -1,10 +1,22 @@
 /**
  * Created by Dima on 04.10.2016.
  */
+
+class Box {
+    int wigth;
+    int higth;
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Box mybox = new Box();
+        mybox.higth = 10;
+        mybox.wigth = 15;
         System.out.println("Hello World!");
+        System.out.print("size of box = " + mybox.higth * mybox.wigth);
     }
 
 }
