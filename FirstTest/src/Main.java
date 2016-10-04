26ad46b45489eab2f04bba8f606f454320a084da
ProforgTDS <formybusiness@mail.ru>
2016-10-04 12:46:06 +0300

    its ok

--- FirstTest/src/Main.java
+++ FirstTest/src/Main.java
@@ -5,6 +5,12 @@
 class Box {
     int wigth;
     int higth;
+
+    void Volume () {
+        System.out.print("size of box =");
+        System.out.println(wigth*higth);
+    }
+
 }
 
 
@@ -13,13 +19,15 @@ public class Main {
     public static void main(String[] args) {
 
         Box mybox = new Box();
-        Box mybox2;
+        Box mybox2 = new Box();
         mybox.higth = 10;
         mybox.wigth = 15;
-        mybox2 = mybox;
-        mybox = null;
+        mybox2.higth = 12;
+        mybox2.wigth = 10;
         System.out.println("Hello World!");
-        System.out.print("size of box = " + mybox2.higth * mybox2.wigth);
+//        System.out.print("size of box = " + mybox2.higth * mybox2.wigth);
+        mybox.Volume();
+        mybox2.Volume();
     }
 
 }
