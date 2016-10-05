9428b02407210560480cc5430508621a1626f10d
ProforgTDS <formybusiness@mail.ru>
2016-10-05 10:03:15 +0300

    its ok

--- FirstTest/src/Main.java
+++ FirstTest/src/Main.java
@@ -2,28 +2,17 @@
  * Created by Dima on 04.10.2016.
  */
 
-class Stack {
-    int stch[] = new int[10];
-    int tos;
+class Test {
+    int a,b;
 
-    Stack(){
-        tos=-1;
+    Test (int i, int j) {
+        a=i;
+        b=j;
     }
 
-    void push (int item) {
-        if (tos==9)
-            System.out.println("Стек заполнен");
-        else
-            stch[++tos]=item;
-    }
-
-    int pop() {
-        if (tos<0) {
-            System.out.println("Стек пуст");
-            return 0;
-        }
-        else
-            return stch[tos--];
+    boolean equals(Test o){
+        if (o.a==a && o.b==b) return true;
+        else return false;
     }
 }
 
@@ -31,16 +20,12 @@ class Stack {
 public class Main {
 
     public static void main(String[] args) {
-        Stack mystack1 = new Stack();
-        Stack mystack2 = new Stack();
-        for (int i=0;i<10;i++) mystack1.push(i);
-        for (int i=10;i<20;i++) mystack2.push(i);
+        Test ob1 = new Test(100,22);
+        Test ob2 = new Test(100,22);
+        Test ob3 = new Test(-1,-1);
 
-        System.out.println("Содержимое стека 1:");
-        for (int i=0;i<10;i++) System.out.print (mystack1.pop() + " ");
-        System.out.println();
-        System.out.println("Содержимое стека 2:");
-        for (int i=10;i<20;i++) System.out.print (mystack2.pop() + " ");
+        System.out.println("ob1==ob2: "+ob1.equals(ob2));
+        System.out.println("ob1==ob3: "+ob1.equals(ob3));
     }
 
 }
